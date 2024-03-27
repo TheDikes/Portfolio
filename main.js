334205e419bc3e4a7a0b8e740c958b6edc2e467c
Cynthia Dike <Maa-mmy>
2024-03-27 19:00:40 +0100

    my portfolio

--- main.js
+++ main.js
@@ -1,72 +1,73 @@
 // toggle icon navbar
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
 
 // scroll section
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 100;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-        if(top >= offset && top < offset + height) {
-            //active navbar links 
-             navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
+    if (top >= offset && top < offset + height) {
+      //active navbar links
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
 
-            // active section for animation on scroll
-            sec.classList.add('show-animate');
-        }
+      // active section for animation on scroll
+      sec.classList.add("show-animate");
+    }
 
-        // if want to use animation that repeats on scroll use this
-        else {
-            sec.classList.remove('show-animate');
-        }
-    });
-//sticky header
+    // if want to use animation that repeats on scroll use this
+    else {
+      sec.classList.remove("show-animate");
+    }
+  });
+  //sticky header
 
-let header = document.querySelector('header');
+  let header = document.querySelector("header");
 
-header.classList.toggle('sticky', window.scrollY > 100);
+  header.classList.toggle("sticky", window.scrollY > 100);
 
-// remove toggles icon and navbar when click navbar links (scroll)
-menuIcon.classList.remove('bx-x');
-navbar.classList.remove('active');
-}
+  // remove toggles icon and navbar when click navbar links (scroll)
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
 
-//toggle content 
+//toggle content
 let showContent = true;
 
 function toggleContent(event) {
-    event.preventDefault(); // Prevent default anchor link behavior
-    showContent = !showContent;
-    const contentDiv = document.querySelector('.p');
-    const btn = document.querySelector('#btn');
-    if (showContent) {
-        contentDiv.style.display = 'block';
-        btn.innerText = 'Read Less'; // Update button text to 'Read Less'
-    } else {
-        contentDiv.style.display = 'none';
-        btn.innerText = 'Read More'; // Update button text to 'Read More'
-    }
+  event.preventDefault(); // Prevent default anchor link behavior
+  showContent = !showContent;
+  const contentDiv = document.querySelector(".p");
+  const btn = document.querySelector("#btn");
+  if (showContent) {
+    contentDiv.style.display = "block";
+    btn.innerText = "Read Less"; // Update button text to 'Read Less'
+  } else {
+    contentDiv.style.display = "none";
+    btn.innerText = "Read More"; // Update button text to 'Read More'
+  }
 }
 
-    // Get current year()
-    const footerDiv = document.getElementById('footer');
+// Get current year()
+const footerDiv = document.getElementById("footer");
 
-    // Get the current year using Date.now() and update the span with id "current-year"
-    const currentYear = new Date().getFullYear();
-    const yearSpan = document.getElementById('current-year');
-    yearSpan.textContent = currentYear;
-
+// Get the current year using Date.now() and update the span with id "current-year"
+const currentYear = new Date().getFullYear();
+const yearSpan = document.getElementById("current-year");
+yearSpan.textContent = currentYear;
